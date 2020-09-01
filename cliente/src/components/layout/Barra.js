feaa32f443e0fb45980b705ff6ad5f0b49fda110
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-09-01 21:07:17 +0200

    Añadir el nombre del usuario que inicio session en la barra

--- cliente/src/components/layout/Barra.js
+++ cliente/src/components/layout/Barra.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+// importamos los context
+import AuthContext from '../../context/autentificacion/authContext';
 
 const Barra = () => {
 
+    // extraer la informacion del context de autentificacion
+    const authContext = useContext(AuthContext);
+    // extraemos la funcion del authcontext
+    const { usuario, fnUsuarioAutenticado } = authContext;
+
+    useEffect(() => {
+        fnUsuarioAutenticado();
+    }, []);
+
     return (
         <header className="app-header">
-            <p className="nombre-usuario">
-                Hola, <span> Victor</span>
-            </p>
+           { usuario && ( <p className="nombre-usuario">
+                Hola, <span> {usuario.nombre} </span>
+            </p>)
+            }
             <nav className="nav-principal">
                 <a href="#!">Cerrar Sesión</a>
             </nav>
